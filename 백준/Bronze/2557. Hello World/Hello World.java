ec25b4e219e4b8856f95883523a89657f357ae0e
지인 <145884442+OziinG@users.noreply.github.com>
2025-05-31 09:56:21 +0900

    [Bronze V] Title: Hello World, Time: 92 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void solution() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
