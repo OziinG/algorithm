a24d6cbe31cd2ce6d43ce195f9737f055387f50b
지인 <145884442+OziinG@users.noreply.github.com>
2025-05-31 09:54:43 +0900

    [Bronze V] Title: 고양이, Time: 88 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() {
+        System.out.println( "\\    /\\\n" + 
+        " )  ( ')\n" + 
+        "(  /  )\n" + 
+        " \\(__)|");    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
