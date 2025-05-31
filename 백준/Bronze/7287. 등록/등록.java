21ffa1367dd686fca98ca40404daa62b8d71dcd8
지인 <145884442+OziinG@users.noreply.github.com>
2025-05-31 09:54:04 +0900

    [Bronze V] Title: 등록, Time: 88 ms, Memory: 13828 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void solution() {
+        System.out.print("54\nwldls9854");
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+}
