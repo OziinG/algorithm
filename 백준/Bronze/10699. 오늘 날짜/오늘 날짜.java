06f00a0f23089d2f47fadeecdf145faa483cf762
지인 <145884442+OziinG@users.noreply.github.com>
2025-05-31 09:54:14 +0900

    [Bronze V] Title: 오늘 날짜, Time: 156 ms, Memory: 17088 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void solution() {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
