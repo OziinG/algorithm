a8233e336f2ea6a25dcb939e15e2880aae45564b
지인 <145884442+OziinG@users.noreply.github.com>
2025-06-09 14:07:46 +0900

    [Bronze IV] Title: Final Price, Time: 212 ms, Memory: 18668 KB -BaekjoonHub

---
+++ 백준/Bronze/28224. Final Price/Final Price.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int sum = 0;
+        for(int i = 1; i <= N; i++){
+            sum += scanner.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
