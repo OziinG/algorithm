1c4d8d89d0a63eb75b81826bfea6dae195fb8c29
지인 <145884442+OziinG@users.noreply.github.com>
2025-08-20 12:41:11 +0900

    [Bronze V] Title: 기찍 N, Time: 1164 ms, Memory: 34004 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        while(N > 0){
+            System.out.println(N--);
+        }
+
+    }
+}
